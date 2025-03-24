b130afb7ef0dd43b1233e6e5b247b4a72613e10e
Ankit Kumar <vickyak1339@gmail.com>
2025-03-24 22:22:06 +0530

    fixed build command

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -13,23 +13,23 @@ function App() {
 
   /////// MAINTAIN SINGLE PAGE FIRST
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const matchdata = await axios.get("https://api.cricapi.com/v1/cricScore?apikey=d02df674-7f65-406a-bb56-f74ae74d7571");
-  //       setItems(matchdata.data.data); // assuming matchdata.data contains the data you need
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //       setError('Failed to fetch match data. Please try again later.');
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   useEffect(() => {
-    setItems(dummyData.data);
+    const fetchData = async () => {
+      try {
+        const matchdata = await axios.get("https://api.cricapi.com/v1/cricScore?apikey=d02df674-7f65-406a-bb56-f74ae74d7571");
+        setItems(matchdata.data.data); // assuming matchdata.data contains the data you need
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Failed to fetch match data. Please try again later.');
+      }
+    };
+
+    fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   setItems(dummyData.data);
+  // }, []);
 
   // console.log(items);
 
